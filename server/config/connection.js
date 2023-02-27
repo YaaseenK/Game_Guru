ec05cead96aa39bb50d330b18374ca6e414569c6
[firstname <yaaseenkhan02@gmail.com>
2023-02-27 14:00:07 -0500

    added line 27 which stopss errors from displaying in the console

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose'); 
 
 // Wrap Mongoose around local connection to MongoDB
- 
     mongoose.connect('mongodb://127.0.0.1:27017/game_guru', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
