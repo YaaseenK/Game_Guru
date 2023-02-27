093dd0f6ebe65ca92bfb0a8958c5d0efb2f87661
[firstname <yaaseenkhan02@gmail.com>
2023-02-27 13:57:56 -0500

    server is connecting

--- server/index.js
+++ server/index.js
@@ -1,5 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const db = require('./config/connection.js') ;
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.set("debug", true);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    })
+});
