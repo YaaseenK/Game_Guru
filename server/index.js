fa05064c3ea8649d0d0faac9a2fb3b7d66d01c1e
ghideluw <ghideluw@gmail.com>
2023-02-27 15:33:04 -0500

    added a route

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const db = require('./config/connection.js') ;
+
+const managerRoutes = require('./routes/manager-routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use('/manager-routes', managerRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.set("debug", true);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    })
+});
+
+mongoose.set('useFindAndModify', false)
